f1836764375272c8375e33bea67597871a46785f
Mark Zwart <mark-me@users.noreply.github.com>
2026-02-02 07:29:06 +0100

    Delete data_reader_test.py

--- app/data_reader_test.py
+++
@@ -1,6 +0,0 @@
-from data_reader import KnaDB
-
-db = KnaDB(dir_resources="/data/kna_resources/", debug=True)
-
-lst_leden = db.voorstelling_info("Hoop Gedoe om Niks - 2011") #("Kinderen van ons Volk - 1938") # #"Assepoes - 2015") #
-lst_leden
